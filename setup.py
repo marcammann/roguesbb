958ca82f9dd53888d1e3ea5d5115c24e6aa9395c
Marc Ammann <marc@yoda.local>
2010-09-09 15:41:22 +0200

    added api and tests and setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "SBB",
+    version = "0.0.1",
+    author = "Peter Manser, Marc Ammann",
+    author_email = "peter.manser@liip.ch, marc@partiql.com",
+    description = (""),
+    license = "Apache License",
+    keywords = "sbb api",
+    url = "http://partiql.com/lab/sbb",
+    packages=['frontend', 'api', 'tests'],
+    long_description=read('README'),
+)
