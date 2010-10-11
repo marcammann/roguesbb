cfe5a981bc03dff75523d6026a55ec421ec5bb44
Peter Manser <mail@petermanser.ch>
2010-10-11 10:59:25 +0200

    url mappings in seperate file for applications

--- sbbfrontend/urls.py
+++ sbbfrontend/urls.py
@@ -1,20 +1,8 @@
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
-    url(r'^$', 'searchform.views.form', name="home"),
-    url(r'^searchform$', 'searchform.views.searchform', name="blah"),
+    (r'^searchform/', include('sbbfrontend.searchform.urls')),
     
     (r'^sitemedia/(?P<path>.*)$', 'django.views.static.serve',
             {'document_root': '/Users/peter/projects/sbb/repos/roguesbb/sbbfrontend/media'}),
-            
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
 )
