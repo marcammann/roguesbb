713f181775449cf90172ea0ad9d86ea1dc806cc9
Peter Manser <mail@petermanser.ch>
2010-10-29 23:45:30 +0200

    export logic from views to controllers. inital commit for structure

--- sbbfrontend/searchform/urls.py
+++ sbbfrontend/searchform/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'', 'searchform.views.searchform'),
+    url(r'stations/get/$1', 'searchform.views.stations'),
 )
--- sbbfrontend/searchform/views.py
+++ sbbfrontend/searchform/views.py
@@ -1,28 +1,9 @@
-from datetime import date, datetime
 
-
-from django.http import HttpResponse
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-from searchform.models import Searchform
-from searchform.controllers.api import SBBApi
-
+#searchform (with response)
 def searchform(request):
-    connections = ''
+    schedules = SchedulesHandler(request)
+    return schedules.render('searchform/form.html')
     
-    if request.method == 'POST':
-        form = Searchform(request.POST)
-        if form.is_valid():
-            # Get connections via API
-            sbbapi = SBBApi('')
-            connections = sbbapi.getConnections(form.cleaned_data['station_from'])
-
-    else:
-        form = Searchform()
-    
-    return render_to_response('searchform/form.html', {
-            'form': form,
-            'connections': connections
-        }, context_instance=RequestContext(request)
-    )
+def getStations(request):
+    stations = StationsHandler
+    return schedules.render('searchform/stations.html')
