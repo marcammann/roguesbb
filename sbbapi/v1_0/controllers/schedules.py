a38723d4f305731cceb17fa3a90d3af894f1df86
Marc Ammann <marc@yoda.local>
2010-11-01 10:25:37 +0100

    datetime and arrival_time are now properly set

--- sbbapi/v1_0/controllers/schedules.py
+++ sbbapi/v1_0/controllers/schedules.py
@@ -1,4 +1,5 @@
 from sbbapi.v1_0.controllers.base import *
+from sbbapi.error import *
 
 from datetime import datetime
 
@@ -27,7 +28,7 @@ class SchedulesController(BaseController):
 	entry_url = 'http://xmlfahrplan.sbb.ch/bin/extxml.exe/'
 	
 	@GET
-	def query(self, request):
+	def query(self, request):		
 		param_means_mask = request.GET.get('means_mask', '1111111111000000')
 		param_departure_id = request.GET.get('departure_id', None)
 		param_arrival_id = request.GET.get('arrival_id', None)
@@ -36,13 +37,26 @@ class SchedulesController(BaseController):
 		param_extensive = bool(request.GET.get('extensive', False))
 		
 		if param_departure_time is None and param_arrival_time is None:
-			param_departure_time = datetime.now()
+			is_arrival_time = False
+			request_time = datetime.now()
+		elif param_departure_time:
+			is_arrival_time = False
+			request_time = datetime.utcfromtimestamp(float(param_departure_time))
+		else:
+			is_arrival_time = True
+			request_time = datetime.utcfromtimestamp(float(param_arrival_time))
 		
-		print repr(request.GET)
+		print repr(request_time)
 		
 		if param_arrival_id is None or param_departure_id is None:
-			# TODO: Error Handling
+			if param_arrival_id is None:
+				raise MissingParameter('arrival_id')
+			if param_departure_id is None:
+				raise MissingParameter('departure_id')
 			return False
+			
+		formatted_date = request_time.strftime('%Y%m%d')
+		formatted_time = request_time.strftime('%H:%M')
 		
 		request_content = """<?xml version="1.0" encoding="iso-8859-1"?>
 <ReqC lang="EN" prod="iPhone3.1" ver="2.3" accessId="MJXZ841ZfsmqqmSymWhBPy5dMNoqoGsHInHbWJQ5PTUZOJ1rLTkn8vVZOZDFfSe">
@@ -57,7 +71,10 @@ class SchedulesController(BaseController):
 		<ReqT a="{is_arrival_time}" date="{formatted_date}" time="{formatted_time}" />
 		<RFlags b="0" f="6" sMode="N" />
 	</ConReq>
-</ReqC>""".format(departure_id=param_departure_id, arrival_id=param_arrival_id, is_arrival_time=0, formatted_date='20101028', formatted_time='10:30', means_mask=param_means_mask)
+</ReqC>""".format(departure_id=param_departure_id, arrival_id=param_arrival_id, is_arrival_time='%i' % (is_arrival_time), formatted_date=formatted_date, formatted_time=formatted_time, means_mask=param_means_mask)
+
+
+		print request_content
 
 		h = Http()
 		resp, content = h.request(self.entry_url, "POST", request_content)
@@ -108,9 +125,6 @@ class SchedulesController(BaseController):
 			'request_id':request_id,
 			'connections':connections,
 		}
-		
-		
-		return "foobar"
 		
 		
 	@GET
